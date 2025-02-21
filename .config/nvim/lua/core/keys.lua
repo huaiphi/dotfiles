778511e93a259a5296bca2aa2d797586f1e3311a
huaiphi <aoninoa@proton.me>
2025-02-22 00:02:42 +0900

    Modify a nvim key binding

--- .config/nvim/lua/core/keys.lua
+++ .config/nvim/lua/core/keys.lua
@@ -1,15 +1,4 @@
 local map = vim.keymap
 local opts = { silent = true, noremap = true }
 
-map.set("n", "<leader>d", function()
-    if #vim.api.nvim_list_wins() == 1 then
-        vim.cmd("DelBuf")
-    else
-        local win_height =
-            vim.api.nvim_win_get_height(vim.api.nvim_get_current_win())
-        vim.cmd("bp | sp | bn | bd")
-        vim.cmd("res " .. win_height)
-    end
-end, opts)
-
 map.set("i", "<C-c>", "<ESC>", opts)
