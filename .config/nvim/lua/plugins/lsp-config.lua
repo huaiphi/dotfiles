79f449afbfc2f1d3fe547ea0e5e5a992d09852ce
huaiphi <aoninoa@proton.me>
2025-03-09 17:26:18 +0900

    Modify nvim LSP config

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -44,19 +44,6 @@ return {
         },
         config = function(_, opts)
             local lspconfig = require("lspconfig")
-            local _border = "single"
-
-            vim.lsp.handlers["textDocument/hover"] =
-                vim.lsp.with(vim.lsp.handlers.hover, {
-                    border = _border,
-                })
-            vim.lsp.handlers["textDocument/signatureHelp"] =
-                vim.lsp.with(vim.lsp.handlers.hover, {
-                    border = _border,
-                })
-            vim.diagnostic.config({
-                float = { border = _border },
-            })
 
             -- from
             -- "https://github.com/neovim/nvim-lspconfig#suggested-configuration"
